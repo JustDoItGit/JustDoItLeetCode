a5695b85255844673f206cec78714d88534f9eff
JustDoIt <cool_kj@126.com>
2021-03-22 23:54:11 +0800

    [283]移动零 最优解法

--- leetcode/editor/cn/[283]移动零.py
+++ leetcode/editor/cn/[283]移动零.py
@@ -22,12 +22,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        j = 0
+        zero = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[j] = nums[i]
-                if i != j:
-                    nums[i] = 0
-                j += 1
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
 
 # leetcode submit region end(Prohibit modification and deletion)
